7613f27fe9e30ae437ebf8c8d2a4ac4e1e6fa469
mybaseball52 <mybaseball52@gmail.com>
2023-04-02 00:41:01 +0800

    add open ai functionality

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const app = express();
 const { LineClient } = require('messaging-api-line');
+const fs = require('fs');
+const { Configuration, OpenAIApi } = require("openai");
+const {spawn} = require('child_process');
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 const client = new LineClient({
   accessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
@@ -40,8 +50,11 @@ app.post('/webhook', async function(req, res) {
   const { events } = body;
 
   if(events && events.length > 0){
-    await client.retrieveMessageContent(events[0].message.id).then((buffer) => {
+    await client.retrieveMessageContent(events[0].message.id).then(async (buffer) => {
       console.log(buffer);
+      const buff = Buffer.from(buffer, 'base64');
+      fs.writeFileSync('test.aac', buff);
+      await transcribe('test.aac');
     })
   }
 
@@ -67,5 +80,34 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.send('error');
 });
+
+async function transcribe(filename) {
+  const openai = new OpenAIApi(configuration);
+  const inputFile = fs.createReadStream(filename);
+  const outputPath = filename.replace('.aac','.mp3');
+  const outputStream = fs.createWriteStream(outputPath);
+  const convertion = spawn("ffmpeg", ["-i","pipe:0","-f","mp3","pipe:1",]);
+  inputFile.pipe(convertion.stdin);
+  convertion.stdout.pipe(outputStream);
+
+  convertion.on('close', async (code) => {
+    let y = fs.createReadStream('./test.mp3')
+    const result = await openai.createTranscription(y, 'whisper-1');
+
+    let improvement = await chatCorrector(result.data.text);
+    let beautify = `origin: ${result.data.text} \n\n improvement: ${improvement}`;
+    await client.pushText(process.env.MY_ACCOUNT, beautify);
+  });
+}
+
+
+async function chatCorrector (input){
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "I want you to act as an English teacher, spelling corrector and improver. Answer the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations, the text is: \n\n" + input 
+  });
+  
+  return completion.data.choices[0].text;
+}
 
 module.exports = app;
