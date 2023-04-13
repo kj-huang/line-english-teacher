8b3407c8259e8cb9feadfd313aa5d75560420ce7
mybaseball52 <mybaseball52@gmail.com>
2023-04-13 21:10:40 +0800

    update docker container

--- smoketest.sh
+++ smoketest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Stop container if exists
+docker stop $(docker ps -q --filter ancestor=demo-line-teacher) && docker rm $(docker ps -aq --filter ancestor=demo-line-teacher)
+
 # Build the Docker image
 docker build -t demo-line-teacher .
 
